1e537346499ba41404f529c38b7882d86256abe8
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-08 00:45:17 +0530

    First commet

---
+++ foodonline_main/urls.py
@@ -0,0 +1,11 @@
+"""
+URL configuration for foodonline_main project.
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+]
