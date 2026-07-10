6fc2dcd4411c73e3107b0f08e9fce26816d29db7
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-11 00:43:06 +0530

    user account

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +107,7 @@ else:
             'PORT': config('DB_PORT', default='5432'),
         }
     }
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
