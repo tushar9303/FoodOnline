7afc420743861a4ec99affb0a5ff3a5233ae902c
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-13 00:19:15 +0530

    add admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User, CustomUserAdmin)
