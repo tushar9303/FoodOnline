ee54472d06962d412dae39686bee3985f1735358
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-12 18:32:26 +0530

    add

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class UserManager(BaseUserManager):
             raise ValueError('User must have an email address')
         
         if not username:
-            raise ValueError('User must have an usernameone')
+            raise ValueError('User must have an username')
         
         user = self.model(
             email = self.normalize_email(email),
@@ -35,40 +35,40 @@ class UserManager(BaseUserManager):
         return user
 
     
-    class User(AbstractBaseUser):
-        RESTAURANT = 1
-        CUSTOMER = 2
+class User(AbstractBaseUser):
+    RESTAURANT = 1
+    CUSTOMER = 2
 
-        ROLE_CHOICES = (
-            (RESTAURANT, 'Restaurant'),
-            (CUSTOMER, 'Customer'),
-        )
-        first_name = models.CharField(max_length=50)
-        last_name = models.CharField(max_length=50) 
-        username = models.CharField(max_length=50, unique=True)
-        email = models.EmailField(unique=True)
-        phone_number = models.CharField(max_length=15, unique=True)
-        role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
+    ROLE_CHOICES = (
+        (RESTAURANT, 'Restaurant'),
+        (CUSTOMER, 'Customer'),
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50) 
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, unique=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
 
-        # required fields
-        date_joined = models.DateTimeField(auto_now_add=True)
-        last_login = models.DateTimeField(auto_now=True)
-        create_date = models.DateTimeField(auto_now_add=True)
-        modified_date = models.DateTimeField(auto_now=True)
-        is_admin = models.BooleanField(default=False)
-        is_staff = models.BooleanField(default=False)
-        is_active = models.BooleanField(default=True) 
-        is_superadmin = models.BooleanField(default=False)
+    # required fields
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True) 
+    is_superadmin = models.BooleanField(default=False)
 
-        USERNAME_FIELD = 'email'
-        REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'phone_number']
 
-        objects = UserManager()
-        def __str__(self):
-            return self.email
+    objects = UserManager()
+    def __str__(self):
+        return self.email
         
-        def has_perm(self, perm, obj=None):
-            return self.is_admin
-        
-        def has_module_perms(self, app_label):
-            return True
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self, app_label):
+        return True
