fae8c18d1ecbf2a801ca86783fd73ca5fdbaa6d5
Tushar9303 <Tushar.sharma9303@gmail.com>
2026-07-16 00:02:08 +0530

    user profile

--- accounts/models.py
+++ accounts/models.py
@@ -83,9 +83,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class userprofile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='userprofile/', blank=True, null=True)
-    cover_photo = models.ImageField(upload_to='userprofile/', blank=True, null=True)
-    address = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='users/profile.pictures/', blank=True, null=True)
+    cover_photo = models.ImageField(upload_to='users/cover_photos/', blank=True, null=True)
+    address_line_1 = models.CharField(max_length=100, blank=True, null=True)
+    address_line_2 = models.CharField(max_length=100, blank=True, null=True)
     country = models.CharField(max_length=15, blank=True, null=True)
     state = models.CharField(max_length=15, blank=True, null=True)
     city = models.CharField(max_length=15, blank=True, null=True)
